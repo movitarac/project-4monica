6b669c2969dcfdf8d7799d10445fe3abe58a6834
movitarac <37436648+movitarac@users.noreply.github.com>
2019-10-18 15:00:36 +0200

    update ecriturecomptable test

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -49,7 +49,7 @@ public class EcritureComptableTest {
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "20", "1"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "30"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
-        assertFalse(vEcriture.isEquilibree(), vEcriture.toString());
+        assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
     }
 
     @Test
